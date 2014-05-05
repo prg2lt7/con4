31f93722be4b0a4151a72056eeaaf69044e56605
daniw <dani.winz@bluewin.ch>
2014-05-05 10:48:29 +0200

    leere Klessen Player und LocalOpponent erstellt

---
+++ software/con4/src/logic/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author daniw
+ */
+public class Player
+{
+    
+}
