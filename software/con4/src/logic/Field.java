68af1171a3a12d9bc2027bc5cecf623df005d730
daniw <dani.winz@bluewin.ch>
2014-05-09 07:31:04 +0200

    Field commented (close #5)

--- software/con4/src/logic/Field.java
+++ software/con4/src/logic/Field.java
@@ -10,10 +10,16 @@ package logic;
  */
 public class Field
 {
+
     private static final int DEFAULTWIDTH = 7;
     private static final int DEFAULTHEIGHT = 6;
     private int[][] field;
 
+    /**
+     * Constructor for a field. 
+     * This constructor uses the default values for width and height of the 
+     * field. 
+     */
     public Field()
     {
         field = new int[DEFAULTWIDTH][DEFAULTHEIGHT];
@@ -25,14 +31,21 @@ public class Field
             }
         }
     }
-    
+
+    /**
+     * Constructor for a field. 
+     * height and width can be specified. If either of them is lower than 4, 
+     * it is replaced with its default value. 
+     * @param width width of the field
+     * @param height height of the field
+     */
     public Field(int width, int height)
     {
-        if (width <= 3)
+        if (width < 4)
         {
             width = DEFAULTWIDTH;
         }
-        if (height <= 3)
+        if (height < 4)
         {
             height = DEFAULTHEIGHT;
         }
@@ -46,22 +59,38 @@ public class Field
         }
     }
 
+    /**
+     * Returns the entire field as a two dimensional array. 
+     * @return field
+     */
     public int[][] getField()
     {
         return field;
     }
-    
+
+    /**
+     * Put one stone in a column.
+     * @param x column in which the stone has to be placed
+     * @param value color of the stone to be inserted, indicates player
+     * @return boolean value that indicates if a stone has been placed
+     *         true if stone is placed in column x
+     *         false if x out of range or if column x was full
+     */
     public boolean putStone(int x, int value)
     {
         int y = 0;
-        while (y < field[x].length && field[x][y] != 0)
+        if (x >= field.length)
         {
-            y++;
-        }
-        if (y < field[x].length)
-        {
-            field[x][y] = value;
-            return true;
+            while (y < field[x].length && field[x][y] != 0)
+            {
+                y++;
+            }
+            if (y < field[x].length)
+            {
+                field[x][y] = value;
+                return true;
+            }
+            return false;
         }
         return false;
     }
