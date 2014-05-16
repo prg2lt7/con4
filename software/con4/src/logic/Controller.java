30cefa93a94fa7197cec54451f5e532dbc691ed4
Fabian <Fabian@Fabian>
2014-05-16 09:23:17 +0200

    created loadGame method - returns game created getGame method - returns game

--- software/con4/src/logic/Controller.java
+++ software/con4/src/logic/Controller.java
@@ -1,15 +1,11 @@
 package logic;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import network.Network;
 
-
 /**
- *class Controller controls the game play.
- *checks if move is valid or invalid.
+ * class Controller controlls the game play.
+ * 
  * @author Fabian
  */
 public class Controller
@@ -25,46 +21,51 @@ public class Controller
     {
         game = new Game();
         opponent = new LocalOpponent(2);
-        
+
     }
-    
+
     /**
-     * Player or opponent intends to set a stone at position x,y
-     * Method checks for possibilty to set stone at requested position
-     * returns if stone could be set to position (true) or not (false)
+     * Player or opponent intends to set a stone at position x,y Method checks
+     * for possibilty to set stone at requested position returns if stone could
+     * be set to position (true) or not (false)
+     *
      * @param xPosition
-     * @return 
+     * @return
      */
     public Game setStone(int xPosition)
     {
         //is field full?
         //checks if there is any free position (0) in a column
         int x = 0;
-        while ((game.getField().getField()[x][game.getField().getField()[x].length - 1] != 0) &&
-                (x < game.getField().getField().length))
+        while ((game.getField().getField()[x][game.getField().getField()[x].length - 1] != 0)
+                && (x < game.getField().getField().length))
         {
             x++;
         }
         game.getField().putStone(xPosition, game.getState().getUserMove());
-        
+
         //if field not full (at least one free position (0) - continue gameplay!
         if (game.getField().getField()[x][game.getField().getField()[x].length - 1] == 0)
         {
             game.getField().putStone(opponent.move(game.getField()), opponent.getValue());
-        }  
-        else //game over (draw)
+        } else //game over (draw)
         {
             game.getState().setGameOver(true);
         }
-        return game; 
-        
+        return game;
+
+    }
+    
+    public Game getGame()
+    {
+        return game;
     }
     
     public void networkGame(Network net, boolean server)
     {
-        
+
     }
-    
+
     public boolean saveGame()
     {
         String fileName = "saveGame.dat";
@@ -73,33 +74,53 @@ public class Controller
         {
             try
             {
-                // Open a file to write game into
+                //Open a file to write game into
                 FileOutputStream saveFile = new FileOutputStream(fileName);
 
-                // Create an ObjectOutputStream to put game-object into file.
+                //Create an ObjectOutputStream to put game-object into file.
                 ObjectOutputStream save = new ObjectOutputStream(saveFile);
 
-                // Save current game to file via FileOutputStream
+                //Save current game to file via FileOutputStream
                 save.writeObject(game);
-                
-                // Close the file.
+
+                //Close the file.
                 save.close();
                 return true;
-            }
-            catch (IOException ex)
+            } catch (IOException ex)
             {
                 System.out.println("File already exists. Creating file failed");
             }
         }
         return false;
     }
-    
+
     public Game loadGame()
     {
+        String fileName = "saveGame.dat";
+        try
+        {
+            //Open file to read from
+            FileInputStream saveFile = new FileInputStream(fileName);
+
+            //Create an ObjectInputStream to get object from saved file.
+            ObjectInputStream save = new ObjectInputStream(saveFile);
+
+            //Restoring game from file. Cast object to Game.
+            game = (Game) save.readObject();
+            
+            //Close the file.
+            save.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace(); // If there was an error, print the info.
+        }
         return game;
+
     }
-    
-    public static void main (String[] args){
+
+    public static void main(String[] args)
+    {
         Controller c = new Controller();
         Game g;
         g = c.setStone(3);
