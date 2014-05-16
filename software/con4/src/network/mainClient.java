17ba7eaeaee4088351e1d8c70211045f54263e1b
Pascal <Pascal@10.3.115.4>
2014-05-16 11:39:50 +0200

    Network: with several Threads
    almost running

--- software/con4/src/network/mainClient.java
+++ software/con4/src/network/mainClient.java
@@ -1,5 +1,7 @@
 package network;
 
+import logic.Controller;
+
 /**
  *
  * @author Pascal Häfliger <pascal.haefliger.01@stud.hslu.ch>
@@ -8,25 +10,23 @@ public class mainClient
 {
     public static void main(String[] args) throws InterruptedException
     {
-        Network client = new Network();
-        
-        System.out.println("Server: staus = "
-                + client.joinGame("localhost")
-                + "\n\n");
+        Network client = new Network(new Controller());
+        client.joinGame("localhost");
 
         Thread t = new Thread(client);
         t.start();
         
         //Ping-pong mit anderer Seite
-        for(int a = 7 ; a > 1 ; a--)
+        for(int a = 7 ; a > 0 ; a--)
         {
             System.out.println("Received: " + client.receMsg()); 
-            t.sleep(1000);
+            t.sleep(39*a+419);
             System.out.println("Sending: " + a);
-            client.sendMsg(a);            
+            client.sendMsg(a);
+            t.sleep(61*a+523);
         }
-        client.stopThread();
         
+        client.stopThread();        
         System.out.print("Connection successfully closed");       
     }
 }
