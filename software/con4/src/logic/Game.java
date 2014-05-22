d8a59a9c566d2f258711d09c261c5d3bcceebff6
Fabian <Fabian@Fabian>
2014-05-22 09:21:58 +0200

    modified game method to serializable to save it unit test controller

--- software/con4/src/logic/Game.java
+++ software/con4/src/logic/Game.java
@@ -1,17 +1,18 @@
 package logic;
 
+import java.io.Serializable;
+
 /**
  * Class Game contains all game parameters.
  * @author Fabian
  */
-public class Game
+public class Game implements Serializable
 {
-
     private static Field field;
     private static State state;
 
     /**
-     * default constructor creates game with default sized field (7*6)
+     * Default constructor. Creates game with default sized field (7*6)
      */
     public Game()
     {
@@ -19,20 +20,31 @@ public class Game
         state = new State();
 
     }
-
+    
+    /**
+     * Custom contructor. Creates game with custom field size
+     * @param width
+     * @param height 
+     */
     public Game(int width, int height)
     {
         field = new Field(width, height);
         state = new State();
     }
 
+    /**
+     * returns field informations of current game
+     * @return 
+     */
     public Field getField()
     {
         return field;
     }
 
-    
-
+    /**
+     * returns game status of current game
+     * @return 
+     */
     public State getState()
     {
         return state;
